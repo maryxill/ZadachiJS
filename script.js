d367ca768859768d75803e12ce2f33cecb1f22b5
rising <maroku1@yandex.ru>
2022-12-22 13:30:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,110 @@
+'use strict';
+//№1
+
+let opearation;
+
+
+console.log(`opearation1 = ${7 + 2} 
+opearation2 = ${7 - 3} 
+opearation3 = ${2 * 4}
+opearation4 = ${4 / 2}`)
+
+//№2
+
+let height = 23
+let width = 10
+console.log(`s = ${height * width}`)
+
+
+//№3
+console.log(`opearation5 = ${5 % 3} 
+opearation6 = ${3 % 5} 
+opearation7 = ${5 + '3'}
+opearation8 = ${'5' - 3}
+opearation9 = ${75 + 'кг'}`)
+
+//№4
+
+let heightC = 10, dc = 4/2,v;
+v = 3.14 * dc**2*heightC;
+console.log(v);
+
+
+//№5
+
+console.log(`opearation10 = ${10 + 2 + ` сумма`} 
+opearation11 = ${10 - 2 + ` разность`} 
+opearation12 = ${10 * 2 + ` произведение`}
+opearation13 = ${10 / 2 + ` частное`}`)
+
+
+//№6
+
+let c = 15
+let d = 2
+let result = c + d
+console.log(result)
+
+
+//№7
+
+console.log(`opearation14 = ${10 + 2 + 5 + `сумма`}`)
+
+
+//№8
+
+let a = 17, b = 10, c2, d2 = 7, result2;
+c2 = a - b;
+d2 = 7;
+result = c2 + d2;
+console.log(result2);
+
+
+//№9
+
+let name1 = 'Илья'
+console.log(`Привет, ${name1}!`);
+
+
+//№10
+
+let age = 18;
+console.log(`Мне ${age} лет!`);
+
+
+//№11
+
+let str = 'adcde'
+console.log(str.charAt(0));
+console.log(str.charAt(2));
+console.log(str.charAt(4));
+
+
+//№12
+
+let s = 1, h, d3, m;
+h = s * 60 * 60;
+d = h * 24;
+m = d3 * 30;
+console.log('Секунд в часе ', h);
+console.log('Секунд в сутках ', d3);
+console.log('Секунд в месяце ', m);
+
+
+//№13
+
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+console.log(num);
+
+
+//№14
+
+let num2 = 10;
+num2 ++;
+num2 ++;
+num2 --;
+console.log(num2);
